cac09f979329f42e97ffbb067427a75cb417e3d5
Muhammad Rafay <rafayniazi962@gmail.com>
2024-09-02 19:05:28 +0500

    Added Some changes

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sliderContainer = document.querySelector(".scroll-bar");
-    const leftArrow = document.querySelector(".fa-arrow-left");
-    const rightArrow = document.querySelector(".fa-arrow-right");
-    const boxWidth = 350; // Adjust this to the actual width of your boxes
-    let numBoxes = 7;
+  const sliderContainer = document.querySelector(".scroll-bar");
+  const leftArrow = document.querySelector(".fa-arrow-left");
+  const rightArrow = document.querySelector(".fa-arrow-right");
+  const boxWidth = 350; // Adjust this to the actual width of your boxes
+  let numBoxes = 7;
 
-    let position = 0;
+  let position = 0;
 
-    rightArrow.addEventListener("click", () => {
-        position -= boxWidth;
-        if (position < -(boxWidth * numBoxes)) {
-            position = 0;
-        }
-        sliderContainer.style.transform = `translateX(${position}px)`;
-    });
+  rightArrow.addEventListener("click", () => {
+    position -= boxWidth;
+    if (position < -(boxWidth * numBoxes)) {
+      position = 0;
+    }
+    sliderContainer.style.transform = `translateX(${position}px)`;
+  });
 
-    leftArrow.addEventListener("click", () => {
-        position += boxWidth;
-        if (position > 0) {
-            position = -(boxWidth * numBoxes);
-        }
-        sliderContainer.style.transform = `translateX(${position}px)`;
-    });
+  leftArrow.addEventListener("click", () => {
+    position += boxWidth;
+    if (position > 0) {
+      position = -(boxWidth * numBoxes);
+    }
+    sliderContainer.style.transform = `translateX(${position}px)`;
+  });
 
-    menuOverlay = document.querySelector(".menu-overlay");
-    hamBurger = document.querySelector(".hamburger");
+  menuOverlay = document.querySelector(".menu-overlay");
+  hamBurger = document.querySelector(".hamburger");
 
-    hamBurger.addEventListener("click", () => {
-        menuOverlay.style.display="block";
-    });
+  hamBurger.addEventListener("click", () => {
+    menuOverlay.style.display = "block";
+  });
 
-    menuOverlay.addEventListener("click", () => {
-        menuOverlay.style.display="none";
-    });
+  menuOverlay.addEventListener("click", () => {
+    menuOverlay.style.display = "none";
+  });
 });
--- login.js
+++ login.js
@@ -1,14 +1,9 @@
-
 document.addEventListener("DOMContentLoaded", function () {
+  const tickBox = document.querySelector("#tickBox");
+  const customCheckmark = document.querySelector(".fa-check");
 
-
-
-    const tickBox = document.querySelector('#tickBox');
-    const customCheckmark = document.querySelector('.fa-check');
-    
-    tickBox.addEventListener('click', () => {
-      customCheckmark.style.display = customCheckmark.style.display === 'block' ? 'none' : 'block';
-    });
-
-
-})
+  tickBox.addEventListener("click", () => {
+    customCheckmark.style.display =
+      customCheckmark.style.display === "block" ? "none" : "block";
+  });
+});
